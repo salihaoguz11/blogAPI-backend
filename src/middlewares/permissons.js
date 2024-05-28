56c3234b319fdca46dc543ef8bb687272ebc4ef3
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2024-05-28 18:39:29 +0100

    permisson middleware completed

---
+++ src/middlewares/permissons.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/* -------------------------------------------------------
+    NODEJS EXPRESS | BlOG API
+------------------------------------------------------- */
+
+// Middleware: permissions
+
+module.exports = {
+    isLogin:(req,res,next)=>{
+    // set passive
+    // return next() -> çalıştırma pass geç
+
+    if(req.user && req.user.isActive){
+        next()
+    }else{
+        res.errorStatusCode = 403;
+        throw new Error("No permisson: You must login");
+
+    }
+
+
+    },
+    isAdmin:(req,res,next)=>{
+    // return next()
+    // only admin
+    if(req.user && req.user?.isAdmin && req.user.isActive){
+        next()
+
+    }else{
+        res.errorStatusCode(403).send("No permission: You must be admin");
+    }
+
+    }
+
+    }
+}
